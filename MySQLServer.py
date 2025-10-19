dd24160af15b4e235edda25b144b523686c421e1
Lekia Serekara <lekiakara@gmail.com>
2025-10-19 21:06:28 +0100

    update

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,7 +17,7 @@ try:
 
         print("Database 'alx_book_store' created successfully (or already exists).")
 
-except Error as e:
+except mysql.connect.Error as e:
     print("Error while connecting to MySQL:", e)
 
 finally:
