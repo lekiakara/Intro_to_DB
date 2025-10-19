f5a9cecd15f9bd3267703ecee7f962ca3bd6dc07
Lekia Serekara <lekiakara@gmail.com>
2025-10-19 20:38:13 +0100

    Add file populate database

--- task_5.sql
+++ task_5.sql
@@ -1,12 +1,7 @@
 use alx_book_store;
 
-INSERT INTO Customer (
-customer_id,
-customer_name,
-email,
-address
-)
-VALUES(
-	1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave."
-    );
+INSERT INTO Customer (customer_id, customer_name, email, address)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
+
+
 
